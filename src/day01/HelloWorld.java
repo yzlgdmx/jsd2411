73006dae12a630e90cb6cc00729a9ed78cbdac6e
yzlgdmx <2944872921@qq.com>
2024-11-27 17:54:37 +0800

    第一天代码

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -1,7 +1,22 @@
 package day01;
 
 public class HelloWorld {
-  public   static void Main(String[] args) {
+    public String str2 = "233";
 
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(233);
+        String str = "这是一个字符串";
+        System.out.println(str);
+        System.out.println(false);
+        enum task {
+            你好,
+            w,
+            sd,
+            sd2
+        }
+        System.out.println(task.你好);
+        HelloWorld s = new HelloWorld();
+        System.out.println(s.str2);
     }
 }
